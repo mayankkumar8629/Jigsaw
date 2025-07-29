c8299911ca4c04ef7751c50917790d209e4681c6
Mayank Kumar <mayankkumarverma306@gmail.com>
2025-07-29 07:11:16 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://jigsaw-pi.vercel.app/', // Your frontend URL
+  origin: 'https://jigsaw-pi.vercel.app', // Your frontend URL
   credentials: true // Required for cookies
 }));
 
